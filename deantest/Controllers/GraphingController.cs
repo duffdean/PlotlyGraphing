6bc567454abaa0c576bef34cc2ea0a1a38950266
duffdean <dean@bmc-ne.co.uk>
2016-09-12 20:48:55 +0100

    https://github.com/duffdean/PlotlyGraphing.git

---
+++ deantest/Controllers/GraphingController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using deantest.Models;
+
+namespace deantest.Controllers
+{
+    public class GraphingController : Controller
+    {
+        private deanduffEntities db = new deanduffEntities();
+
+        // GET: Graphing
+        public ActionResult Index()
+        {
+            return View(db.dummies.ToList());
+        }
+
+        // GET: Graphing/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            dummy dummy = db.dummies.Find(id);
+            if (dummy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dummy);
+        }
+
+        // GET: Graphing/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Graphing/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "testID,name,x,y")] dummy dummy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.dummies.Add(dummy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dummy);
+        }
+
+        // GET: Graphing/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            dummy dummy = db.dummies.Find(id);
+            if (dummy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dummy);
+        }
+
+        // POST: Graphing/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "testID,name,x,y")] dummy dummy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dummy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dummy);
+        }
+
+        // GET: Graphing/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            dummy dummy = db.dummies.Find(id);
+            if (dummy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dummy);
+        }
+
+        // POST: Graphing/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            dummy dummy = db.dummies.Find(id);
+            db.dummies.Remove(dummy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ deantest/Controllers/dummyAPIController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using deantest.Models;
+
+namespace deantest.Controllers
+{
+    public class dummyAPIController : ApiController
+    {
+        private deanduffEntities db = new deanduffEntities();
+
+        // GET: api/dummyAPI
+        public IQueryable<dummy> Getdummies()
+        {
+            return db.dummies;
+        }
+
+        // GET: api/dummyAPI/5
+        [ResponseType(typeof(dummy))]
+        public IHttpActionResult Getdummy(int id)
+        {
+            dummy dummy = db.dummies.Find(id);
+            if (dummy == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dummy);
+        }
+
+        // PUT: api/dummyAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putdummy(int id, dummy dummy)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != dummy.testID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(dummy).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!dummyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/dummyAPI
+        [ResponseType(typeof(dummy))]
+        public IHttpActionResult Postdummy(dummy dummy)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.dummies.Add(dummy);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = dummy.testID }, dummy);
+        }
+
+        // DELETE: api/dummyAPI/5
+        [ResponseType(typeof(dummy))]
+        public IHttpActionResult Deletedummy(int id)
+        {
+            dummy dummy = db.dummies.Find(id);
+            if (dummy == null)
+            {
+                return NotFound();
+            }
+
+            db.dummies.Remove(dummy);
+            db.SaveChanges();
+
+            return Ok(dummy);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool dummyExists(int id)
+        {
+            return db.dummies.Count(e => e.testID == id) > 0;
+        }
+    }
+}
